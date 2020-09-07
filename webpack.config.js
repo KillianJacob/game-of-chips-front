84b25a6922bbb4c47fb87ba74f8b3fa4cefe4140
Killian Jacob <jacob.killian@outlook.fr>
2020-09-07 13:28:45 +0200

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: "./loader.js",
     mode: "production",
-    watch : true,
     output: {
         path: path.resolve("./"),
         filename : "loader.min.js"
